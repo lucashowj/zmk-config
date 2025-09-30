6c932ed7d070a3b7e6670f4575144cd7e0aa0761
lucashowj <41644260+lucashowj@users.noreply.github.com>
2025-09-30 15:12:09 +0800

    Add files via upload

---
+++ boards/shields/polilla/board.cmake
@@ -0,0 +1,10 @@
+# Polilla board configuration
+
+# Copyright (c) 2020 The ZMK contributors
+# SPDX-License-Identifier: MIT
+
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
